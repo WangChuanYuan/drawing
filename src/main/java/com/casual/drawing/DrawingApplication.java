366d0658b9a4e39995a82e71656a4af169273268
161250135 <161250135@smail.nju.edu.cn>
2018-09-09 10:49:43 +0800

    项目配置

--- src/main/java/com/casual/drawing/DrawingApplication.java
+++ src/main/java/com/casual/drawing/DrawingApplication.java
@@ -2,9 +2,16 @@ package com.casual.drawing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DrawingApplication {
+public class DrawingApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+		return springApplicationBuilder.sources(this.getClass());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrawingApplication.class, args);
