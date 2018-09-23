186f1116d32a53efd64b269516be2cbbda31cd4b
161250135 <161250135@smail.nju.edu.cn>
2018-09-23 21:54:13 +0800

    修正连续读取同一张图片的bug

--- src/main/resources/static/js/import-img.js
+++ src/main/resources/static/js/import-img.js
@@ -16,6 +16,8 @@ var importImg = function (canvasId, fileInputId) {
                 //将图片从(0, 0)至(width,height) 画入 canvas(0,0)至(width,height)
                 context.drawImage(image, 0, 0, image.width, image.height, 0, 0, pane.width, pane.height);
             }
+            $(fileInputId).val(""); //清除读取内容，以便连续读取同一张图片
+            window.imgName = "";
         }
     }
 };
